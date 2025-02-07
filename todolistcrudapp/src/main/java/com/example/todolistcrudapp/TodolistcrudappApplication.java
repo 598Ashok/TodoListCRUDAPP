e2f2f2e3fc2f8a972d872073be707c6cd567b518
Myla Ashok <100290801+598Ashok@users.noreply.github.com>
2025-02-07 12:57:57 +0530

    Add files via upload

---
+++ todolistcrudapp/src/main/java/com/example/todolistcrudapp/TodolistcrudappApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistcrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistcrudappApplication.class, args);
+	}
+
+}
---
+++ todolistcrudapp/src/test/java/com/example/todolistcrudapp/TodolistcrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistcrudappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
